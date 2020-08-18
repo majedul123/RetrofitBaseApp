a204fb7247948c168baa46eef03d9048d0ff225b
majedul <majedict34@gmail.com>
2020-08-18 11:45:40 +0600

    initial commit

---
+++ app/src/main/java/com/majedul/surveyapp/MainActivity.java
@@ -0,0 +1,14 @@
+package com.majedul.surveyapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
